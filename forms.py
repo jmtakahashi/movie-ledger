a5cceb452bad3c6653798f0fcb010fe39bdcb52f
Jason Takahashi <jmtakahashi@gmail.com>
2025-07-07 08:31:29 -0700

    change DataRequired validator to InputRequired. removed length validator on password fields except or the signup password field.  add length validator to signup password field.

--- forms.py
+++ forms.py
@@ -1,45 +1,46 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, HiddenField, BooleanField, SelectField, DateField, RadioField
+"""Forms for our Flask app"""
 
-from wtforms.validators import DataRequired, Email, Length, Optional
-import email_validator
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, HiddenField, BooleanField, SelectField, DateField
+
+from wtforms.validators import InputRequired, Email, Length, Optional
 
 
 class UserAddForm(FlaskForm):
     """Form for adding users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[
+                           InputRequired(), Length(min=3)])
+    email = StringField('E-mail', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[
-                             DataRequired(), Length(min=6)])
+                             InputRequired(), Length(min=6)])
     # img_url = StringField('(Optional) Image URL')
 
 
 class LoginForm(FlaskForm):
     """Login form."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[
-                             DataRequired(), Length(min=6)])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
 
 
 class UserEditForm(FlaskForm):
     """Form for editing users."""
 
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[InputRequired()])
+    email = StringField('E-mail', validators=[InputRequired(), Email()])
     new_password = PasswordField('New Password', validators=[
                                  Optional(), Length(min=6)])
     img_url = StringField('(Optional) Image URL')
     password = PasswordField('Current Password', validators=[
-                             DataRequired(), Length(min=6)])
+                             InputRequired()])
 
 
 class UserDeleteForm(FlaskForm):
     """Movie search form."""
 
     password = PasswordField('Enter Password to Confirm', validators=[
-                             DataRequired(), Length(min=6)])
+                             InputRequired()])
 
 
 class MovieAddEditForm(FlaskForm):
